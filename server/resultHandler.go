c941098496b8850f5f94bbb694b0577cf353df3c
Vivek Vasvani <vivekvasvani@Viveks-MacBook-Pro.local>
2017-04-27 15:32:38 +0530

    fix

--- server/resultHandler.go
+++ server/resultHandler.go
@@ -15,12 +15,9 @@ const (
 )
 
 var (
-	header     = make(map[string]string)
-	output     = make([]string, 0)
-	release    = make([]string, 0)
-	userId     string
-	wait       string
-	jenkinsUrl = "https://jenkins.im.hike.in:8443/view/Release%20(Integration)/job/android_on_demand_build/buildWithParameters?token=AaBbCcDd12345&origin=${0}&Branch=${1}&Build_Flavour=${2}&Theme=${3}&Slack_Notification=${4}"
+	header  = make(map[string]string)
+	output  = make([]string, 0)
+	release = make([]string, 0)
 )
 
 func getSTGStatus(ctx *fasthttp.RequestCtx) {
@@ -78,16 +75,29 @@ func sendOptions(ctx *fasthttp.RequestCtx) {
 	var (
 		responseURL string
 	)
+	//output slice before making call
+	output = make([]string, 0)
+	release = make([]string, 0)
+
+	//headers for response
 	header["Content-Type"] = application_json
 	header["Accept"] = application_json
+
+	//get Response URL from request
 	responseURL = string(ctx.PostArgs().Peek("response_url"))
 	//channelId = string(ctx.PostArgs().Peek("channel_id"))
+
+	//To get command line args
 	text := string(ctx.PostArgs().Peek("text"))
 	if len(strings.Split(text, " ")) != 2 {
 		client.HitRequest(responseURL, "POST", header, " { \"text\" : \" Command line params are missing Expected /androidbuild [origin] [branchname]\" } ")
 	}
+
+	//Saving values of origin and branch name
 	output = append(output, strings.Split(text, " ")[0])
 	output = append(output, strings.Split(text, " ")[1])
+
+	//Send back the response/options to user
 	client.HitRequest(responseURL, "POST", header, SubstParams([]string{"\n" + strings.Join(release[:], ",")}, GetPayload("sendOptions.json")))
 }
 
@@ -117,6 +127,7 @@ func sendMoreOptions(ctx *fasthttp.RequestCtx) {
 
 		case requestPayloadButton.Actions[0].Type == "button" && requestPayloadButton.Actions[0].Value == "done":
 			client.HitRequest(requestPayloadButton.ResponseURL, "POST", header, SubstParams(output, GetPayload("finalResponseAfterSubmit.json")))
+			jenkinsUrl := "https://jenkins.im.hike.in:8443/view/Release%20(Integration)/job/android_on_demand_build/buildWithParameters?token=AaBbCcDd12345&origin=${0}&Branch=${1}&Build_Flavour=${2}&Theme=${3}&Slack_Notification=${4}"
 			output = append(output, "@"+requestPayloadButton.User.Name)
 			payload := SubstParams(output, GetPayload("jenkins.json"))
 			jenkinsUrl = SubstParams(output, jenkinsUrl)
